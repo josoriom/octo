1524ead80829781129caff79a55fde27b38bb93a
josoriom <josoriom93@gmail.com>
2026-01-27 14:29:54 +0800

    docs: update docs

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -1,5 +1,5 @@
 pub mod mzml;
-pub use mzml::{parse_mzml, structs::*};
+pub use mzml::{bin_to_mzml, parse_mzml, structs::*};
 pub mod b64;
 pub use b64::{decode, encode, utilities::Header};
 pub mod utilities;
